e004f8e4a803fb9a269f6de8907a2cf9e66f8e5c
weiguanjie <405674833@qq.com>
2019-10-12 19:08:32 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -1,27 +1,28 @@
-  document.write("<p>金字塔</p>")
-  let level = prompt('请设置金字塔的层数')
-  level= parseFloat(level)&&Number(level)
-  if (isNaN (leve)) {
-    alert('金字塔的层数是数学')
-  }
-  for(let i=1;i<=level;++i){
-      let blank = level - i;
-      for(let k=0;k<blank;++k){
-      document.write('&nbsp;');
-  }
-  let star = i * 2 -1
-  for (let j=0;j<star;++j){
-      domcument.write('*')
-  }
-  document.write("<br>")
+document.write("<p>金字塔</p>")
+let level = prompt('请设置金字塔的层数')
+level = parseFloat(level)&&Number(level)
+if (isNaN(level)){
+    alert('金字塔的层数是数字')
 }
-let str ='<table border="1">'
-for(let i=1;i<10;++i){
-    str +='<tr>'
-    for(var j=1;j<=i;++j){
-        str +='<td>' + '*' +i + '=' +(j*i)+'/td'
+for(let i=1;i<=level;++i){
+    let blank = level - i;
+    for(let k=0;k<blank;++k){
+        document.write('&nbsp;');
     }
-    str +='</tr>'
+    let star = i * 2 - 1
+    for(let j=0;j<star;++j){
+        document.write('*')
+    }
+    document.write("<br>")
 }
-str+='</table>'
+
+let str = '<table border="1">'
+for(let i=1;i<10;i++){
+    str += '<tr>'
+    for(var j=1;j<=i;++j){
+        str += '<td>' + j + '*' + i + '=' + ( j * i ) + '</td>' 
+    }
+    str += '</tr>'
+}
+str += '</table>'
 document.getElementById('table').innerHTML = str
