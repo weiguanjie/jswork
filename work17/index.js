e46830e90e1bc2fda213991b9e2d9f4af5d3a0b5
weiguanjie <405674833@qq.com>
2019-12-21 11:57:26 +0800

    work17

--- work17/index.js
+++ work17/index.js
@@ -1,5 +1,5 @@
 var inputs = document.getElementsByTagName('input');
-for (var i = 0;i < inputs.length - 1; ++i) {
+for (var i = 0; i < inputs.length - 1; ++i) {
     inputs[i].onblur = inputBlur;
 }
 function inputBlur() {
@@ -9,48 +9,50 @@ function inputBlur() {
     var tips_obj = this.parentNode.nextElementSibling;
     val = val.trim();
     if (!val) {
-        error(tips_obj, '输入框不能为空');
+        error(tips_obj, '输入框不能为空')
         return false;
     }
-    var reg_msg = getRegMsg(name, tips);
-    if(reg_msg['reg'].test(val)) {
-        success(tips_obj, reg_msg['msg']['success']);
-    }else{
-        error(tips_obj, reg_msg['msg']['error']);
+    var reg_msg = getRegMsg(name, tips)
+    if (reg_msg['reg'].test(val)) {
+        success(tips_obj, reg_msg['msg']['success'])
+    } else {
+        error(tips_obj, reg_msg['msg']['error'])
     }
 }
+
 function getRegMsg(name, tips) {
     var reg = msg = '';
-    switch (name)  {
+    switch (name) {
         case 'username':
-            reg = /^[a-zA-Z]{4,12}$/;
-        msg = {'success': '用户名输入正确','error': tips};
-        break;
+            reg = /^[a-zA-Z]{4,12}$/
+            msg = {'success': '用户名输入正确', 'error': tips};
+            break;
         case 'pwd':
             reg = /^\w{6,20}$/;
-            msg ={'success': '密码输入正确', 'error': tips};
+            msg = {'success': '密码输入正确', 'error': tips};
             break;
-            case 'repwd':
+        case 'repwd':
             var con = document.getElementsByTagName('input')[1].value;
             reg = RegExp("^" + con + "$");
             msg = {'success': '两次密码输入正确', 'error': '两次输入的密码不一致'};
             break;
-            case 'tel':
-                reg = /^1[34578]\d{9}$/;
-                msg = {'success': '手机号码输入正确', 'error': tips};
+        case 'tel':
+            reg = /^1[34578]\d{9}$/;
+            msg = {'success': '手机号码输入正确', 'error': tips};
             break;
-            case 'email':
-                reg = /^(\w+(\_|\-|\.)*)+@(\w+(\-)?)+(\.\w{2,})+$/;
-                msg = {'success': '邮箱输入正确', 'error': tips};
-                break;
+        case 'email':
+            reg = /^(\w+(\_|\-|\.)*)+@(\w+(\-)?)+(\.\w{2,})+$/;
+            msg = {'success': '邮箱输入正确', 'error': tips};
+            break;
     }
-    return {'reg': reg, "msg": msg};
-    }
-    function success(obj, msg) {
-        obj.className = 'success';
-        obj.innerHTML = msg;
-    }
-    function error(obj, msg) {
-        obj.className = 'error';
-        obj.innerHTML = msg + ', 请重新输入';
-    }
+    return {'reg': reg, 'msg': msg}
+}
+
+function success(obj, msg) {
+    obj.className = 'success'
+    obj.innerHTML = msg
+}
+function error(obj, msg) {
+    obj.className = 'error'
+    obj.innerHTML = msg + '，请重新输入'
+}
